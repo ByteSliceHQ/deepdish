0a24d8d70273af7627f865ae532a0fb692c4ded3
Dylan Slack <dylanslack@gmail.com>
2024-06-19 15:01:24 -0600

    chore: fix lint issues again

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import '../globals.css'
 
+import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils'
 
 const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
