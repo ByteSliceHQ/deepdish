6e7a26441fedfa8fecf3779d847097704d3a0e2e
J. Eric Bukowski <eric@byteslice.co>
2024-07-14 11:21:51 -0600

    feat(marketing): group media elements

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -16,20 +16,26 @@ export default function Page() {
           pizza powers
         </Link>
       </Paragraph>
-      <Image
-        alt="Neon sign hanging outside of a pizza parlor"
-        src="https://images.unsplash.com/photo-1546724867-3b2dabdbc5b0?h=300"
-        title="Pizza parlor sign"
-      />
-      <Audio src="https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg">
-        Pizza induced laughter
-      </Audio>
-      <Video
-        height={250}
-        src="https://videos.pexels.com/video-files/3752507/3752507-hd_1920_1080_24fps.mp4"
-      >
-        Deliciously cheesy stock footage of pepperoni pizza
-      </Video>
+      <div>
+        <Image
+          alt="Neon sign hanging outside of a pizza parlor"
+          src="https://images.unsplash.com/photo-1546724867-3b2dabdbc5b0?h=300"
+          title="Pizza parlor sign"
+        />
+      </div>
+      <div>
+        <Audio src="https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg">
+          Pizza induced laughter
+        </Audio>
+      </div>
+      <div>
+        <Video
+          height={250}
+          src="https://videos.pexels.com/video-files/3752507/3752507-hd_1920_1080_24fps.mp4"
+        >
+          Deliciously cheesy stock footage of pepperoni pizza
+        </Video>
+      </div>
     </>
   )
 }
