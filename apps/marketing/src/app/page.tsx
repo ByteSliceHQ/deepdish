b0ea5e445d65e6aa1f38d2918b32593511baa7ba
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-07-16 15:58:11 -0600

    feat(ui): init "drop-in" components of basic modules (#8)

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -1,10 +1,41 @@
-import { Text } from '@deepdish/ui/text'
+import { Link } from '@deepdish/ui/link'
+import { Audio, Image, Video } from '@deepdish/ui/media'
+import { Heading, Paragraph } from '@deepdish/ui/typography'
 
 export default function Page() {
   return (
     <>
-      Page
-      <Text />
+      <Heading level={2}>DeepDish</Heading>
+      <Paragraph>
+        Headless CMS with&nbsp;
+        <Link
+          href="https://byteslice.co"
+          target="_blank"
+          title="ByteSlice Homepage"
+        >
+          pizza powers
+        </Link>
+      </Paragraph>
+      <div>
+        <Image
+          alt="Neon sign hanging outside of a pizza parlor"
+          src="https://images.unsplash.com/photo-1546724867-3b2dabdbc5b0?h=300"
+          title="Pizza parlor sign"
+        />
+      </div>
+      <div>
+        <Audio src="https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg">
+          Pizza induced laughter
+        </Audio>
+      </div>
+      <div>
+        <Video
+          height={250}
+          src="https://videos.pexels.com/video-files/3752507/3752507-hd_1920_1080_24fps.mp4"
+        >
+          Deliciously cheesy stock footage of pepperoni pizza
+        </Video>
+      </div>
     </>
   )
 }
