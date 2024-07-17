e7675108eeb9d4b38cd2d5ccd795cbd55c3bf6e5
J. Eric Bukowski <eric@byteslice.co>
2024-07-17 11:27:52 -0600

    feat: export individual heading components

--- packages/ui/src/typography.tsx
+++ packages/ui/src/typography.tsx
@@ -23,6 +23,30 @@ export function Heading(props: HeadingProps & { level: HeadingLevel }) {
   return <Component {...rest}>{content}</Component>
 }
 
+export function Heading1(props: HeadingProps) {
+  return <Heading level={1} {...props} />
+}
+
+export function Heading2(props: HeadingProps) {
+  return <Heading level={2} {...props} />
+}
+
+export function Heading3(props: HeadingProps) {
+  return <Heading level={3} {...props} />
+}
+
+export function Heading4(props: HeadingProps) {
+  return <Heading level={4} {...props} />
+}
+
+export function Heading5(props: HeadingProps) {
+  return <Heading level={5} {...props} />
+}
+
+export function Heading6(props: HeadingProps) {
+  return <Heading level={6} {...props} />
+}
+
 type ParagraphProps = JSX.IntrinsicElements['p']
 
 export function Paragraph(props: ParagraphProps) {
