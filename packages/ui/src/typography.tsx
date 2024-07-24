e8aeba3aa9729d3293b29b7c50311edc44b8ec00
J. Eric Bukowski <eric@byteslice.co>
2024-07-24 13:21:13 -0600

    feat: add additional components

--- packages/ui/src/typography.tsx
+++ packages/ui/src/typography.tsx
@@ -1,5 +1,41 @@
 type TypographyProps<T> = Omit<T, 'children'> & { children: string }
 
+type BlockQuoteProps = TypographyProps<JSX.IntrinsicElements['blockquote']>
+
+export function BlockQuote(props: BlockQuoteProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <blockquote>{content}</blockquote>
+}
+
+type BoldProps = TypographyProps<JSX.IntrinsicElements['b']>
+
+export function Bold(props: BoldProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <b>{content}</b>
+}
+
+type DivProps = TypographyProps<JSX.IntrinsicElements['div']>
+
+export function Div(props: DivProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <div>{content}</div>
+}
+
+type EmphasizeProps = TypographyProps<JSX.IntrinsicElements['em']>
+
+export function Emphasize(props: EmphasizeProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <em>{content}</em>
+}
+
 const headings = {
   1: 'h1',
   2: 'h2',
@@ -49,6 +85,15 @@ export function Heading6(props: HeadingProps) {
   return <Heading level={6} {...props} />
 }
 
+type ItalicizeProps = TypographyProps<JSX.IntrinsicElements['i']>
+
+export function Italicize(props: ItalicizeProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <i>{content}</i>
+}
+
 type ParagraphProps = TypographyProps<JSX.IntrinsicElements['p']>
 
 export function Paragraph(props: ParagraphProps) {
@@ -56,4 +101,31 @@ export function Paragraph(props: ParagraphProps) {
   const content = props.children
 
   return <p {...props}>{content}</p>
+}
+
+type SpanProps = TypographyProps<JSX.IntrinsicElements['span']>
+
+export function Span(props: SpanProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <span>{content}</span>
+}
+
+type StrongProps = TypographyProps<JSX.IntrinsicElements['strong']>
+
+export function Strong(props: StrongProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <strong>{content}</strong>
+}
+
+type UnderlineProps = TypographyProps<JSX.IntrinsicElements['u']>
+
+export function Underline(props: UnderlineProps) {
+  // TODO: preferentially load from CMS
+  const content = props.children
+
+  return <u>{content}</u>
 }
