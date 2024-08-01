4f15832b1364c75e3eabeab4f4757d4850f5f9ce
J. Eric Bukowski <eric@byteslice.co>
2024-08-01 17:28:08 -0600

    feat(ui): hook up link module

--- packages/ui/src/link.tsx
+++ packages/ui/src/link.tsx
@@ -1,20 +1,29 @@
+import { DeepDish } from './deepdish'
 import type { ElementProps } from './types'
 
 type LinkValue = {
-  destination: string
-  href: string
-  title: string
+  destination?: string
+  href?: string
+  title?: string
 }
 
 export function Link(props: ElementProps<'a', LinkValue, string>) {
-  // TODO: preferentially load from CMS
-  const destination = props.children
-  const href = props.href
-  const title = props.title
-
   return (
-    <a {...props} href={href} title={title}>
-      {destination}
-    </a>
+    // @ts-expect-error Server Component
+    <DeepDish
+      deepdish={props.deepdish}
+      fallback={{
+        destination: props.children,
+        href: props.href,
+        title: props.title,
+      }}
+      render={(content) => {
+        return (
+          <a {...props} href={content?.href} title={content?.title}>
+            {content?.destination}
+          </a>
+        )
+      }}
+    />
   )
 }
