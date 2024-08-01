69057b746937c8472faf75b50d927f60478ab0e3
J. Eric Bukowski <eric@byteslice.co>
2024-08-01 17:38:15 -0600

    feat(ui): hook up media module

--- packages/ui/src/media.tsx
+++ packages/ui/src/media.tsx
@@ -1,50 +1,82 @@
+import { DeepDish } from './deepdish'
 import type { ElementProps } from './types'
 
 type AudioValue = {
-  fallback: string
-  src: string
+  fallback?: string
+  src?: string
 }
 
 export function Audio(props: ElementProps<'audio', AudioValue, string>) {
-  // TODO: preferentially load from CMS
-  const fallback = props.children
-  const src = props.src
-
   return (
-    <audio controls {...props} src={src}>
-      {fallback}
-    </audio>
+    // @ts-expect-error Server Component
+    <DeepDish
+      deepdish={props.deepdish}
+      fallback={{
+        fallback: props.children,
+        src: props.src,
+      }}
+      render={(content) => {
+        return (
+          <audio controls {...props} src={content?.src}>
+            {content?.fallback}
+          </audio>
+        )
+      }}
+    />
   )
 }
 
 type ImageValue = {
-  description: string
-  src: string
-  title: string
+  description?: string
+  src?: string
+  title?: string
 }
 
 export function Image(props: ElementProps<'img', ImageValue>) {
-  // TODO: preferentially load from CMS
-  const description = props.alt
-  const src = props.src
-  const title = props.title
-
-  return <img {...props} alt={description} src={src} title={title} />
+  return (
+    // @ts-expect-error Server Component
+    <DeepDish
+      deepdish={props.deepdish}
+      fallback={{
+        description: props.alt,
+        src: props.src,
+        title: props.title,
+      }}
+      render={(content) => {
+        return (
+          <img
+            {...props}
+            alt={content?.description}
+            src={content?.src}
+            title={content?.title}
+          />
+        )
+      }}
+    />
+  )
 }
 
 type VideoValue = {
-  fallback: string
-  src: string
+  fallback?: string
+  src?: string
 }
 
 export function Video(props: ElementProps<'video', VideoValue, string>) {
-  // TODO: preferentially load from CMS
-  const fallback = props.children
-  const src = props.src
-
   return (
-    <video controls {...props} src={src}>
-      {fallback}
-    </video>
+    // @ts-expect-error Server Component
+    <DeepDish
+      deepdish={props.deepdish}
+      fallback={{
+        fallback: props.children,
+        src: props.src,
+      }}
+      render={(content) => {
+        return (
+          <video controls {...props} src={content?.src}>
+            {content?.fallback}
+          </video>
+        )
+      }}
+    />
   )
 }
