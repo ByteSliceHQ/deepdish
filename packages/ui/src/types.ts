66676a139777c15d617a010d90df88e25b4309e5
J. Eric Bukowski <eric@byteslice.co>
2024-08-01 16:14:03 -0600

    feat(ui): export deep dish props

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -4,10 +4,12 @@ export type IntrinsicElement = keyof JSX.IntrinsicElements
 
 type SetChildren<T, C> = Omit<T, 'children'> & { children?: C }
 
+export type DeepDishProps<V> = { key: string; resolver: Resolver<V> }
+
 export type ElementProps<
   E extends IntrinsicElement,
   V,
   C = undefined,
 > = SetChildren<JSX.IntrinsicElements[E], C> & {
-  deepdish?: { key: string; resolver: Resolver<V> }
+  deepdish?: DeepDishProps<V>
 }
