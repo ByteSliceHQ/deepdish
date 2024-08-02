0570829bc87f484a474791a16e6e79317cf9319f
J. Eric Bukowski <eric@byteslice.co>
2024-08-02 13:29:43 -0600

    feat(ui): wrap read operation

--- packages/ui/src/deepdish.tsx
+++ packages/ui/src/deepdish.tsx
@@ -6,11 +6,17 @@ export async function DeepDish<V>(props: {
   render: (value?: V) => React.ReactNode
 }) {
   if (props.deepdish) {
-    const { key, resolver } = props.deepdish
-    const value = await resolver.read({ key })
+    try {
+      const { key, resolver } = props.deepdish
+      const value = await resolver.read({ key })
 
-    if (value) {
-      return props.render(value)
+      if (value) {
+        return props.render(value)
+      }
+
+      // TODO: handle "missing" data
+    } catch (err) {
+      // TODO: handle error
     }
   }
 
