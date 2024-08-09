caceef8da1bb67f165cd933f19e76d8546cb97ab
J. Eric Bukowski <eric@byteslice.co>
2024-08-09 12:14:28 -0600

    feat(ui): initialize configuration structure

--- packages/ui/src/config.ts
+++ packages/ui/src/config.ts
@@ -1,13 +1,34 @@
-type Config = null
+import type { Resolver } from '@deepdish/resolvers'
+import type { LinkValue } from './link'
+import type { AudioValue, ImageValue, VideoValue } from './media'
+import type { TypographyValue } from './typography'
+
+type Config = {
+  audio?: {
+    resolver: Resolver<AudioValue>
+  }
+  image?: {
+    resolver: Resolver<ImageValue>
+  }
+  link?: {
+    resolver: Resolver<LinkValue>
+  }
+  typography?: {
+    resolver: Resolver<TypographyValue>
+  }
+  video?: {
+    resolver: Resolver<VideoValue>
+  }
+}
 
 let config: Config
 
-export function makeConfig(): void {
+export function makeConfig(input: Config): void {
   if (config) {
     throw Error('Configuration has already been initialized')
   }
 
-  config = null
+  config = Object.freeze(input)
 }
 
 export function getConfig(): Config {
