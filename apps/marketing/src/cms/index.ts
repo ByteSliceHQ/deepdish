69a888af161131cd662b6bcd7effae1086efde03
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-08-20 09:59:29 -0600

    refactor(ui): partition configuration (#36)

--- apps/marketing/src/cms/index.ts
+++ apps/marketing/src/cms/index.ts
@@ -1,16 +1,18 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { createJsonResolver } from '@deepdish/resolvers/json'
-import { makeConfig } from '@deepdish/ui/config'
+import { configure } from '@deepdish/ui/config'
 import { typographySchema } from '@deepdish/ui/typography'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const jsonPath = path.resolve(__dirname, './data.json')
 
 export function cms() {
-  makeConfig({
-    typography: {
-      resolver: createJsonResolver(typographySchema, jsonPath),
+  configure({
+    contracts: {
+      typography: {
+        resolver: createJsonResolver(typographySchema, jsonPath),
+      },
     },
   })
 }
