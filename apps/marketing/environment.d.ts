b424e8d8b34f2315c6338e51e012269b86fa9690
Dylan Slack <dylanslack@gmail.com>
2024-08-21 14:00:34 -0600

    feat: add DeepDish Cloud resolvers integration

---
+++ apps/marketing/environment.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DEEPDISH_SECRET_KEY: string
+    DEEPDISH_URL: string
+  }
+}
--- apps/marketing/src/cms/index.ts
+++ apps/marketing/src/cms/index.ts
@@ -1,17 +1,11 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { createJsonResolver } from '@deepdish/resolvers/json'
+import { createTypographyResolver } from '@deepdish-cloud/resolvers/typography'
 import { configure } from '@deepdish/ui/config'
-import { typographySchema } from '@deepdish/ui/typography'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const jsonPath = path.resolve(__dirname, './data.json')
-
-export function cms() {
+export function cms(url: string, apiKey: string) {
   configure({
     contracts: {
       typography: {
-        resolver: createJsonResolver(typographySchema, jsonPath),
+        resolver: createTypographyResolver(url, apiKey),
       },
     },
   })
