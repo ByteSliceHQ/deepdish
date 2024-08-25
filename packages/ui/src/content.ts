cf05df29582aaf8f43d65ddec17ca13be874ed7f
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-08-25 09:41:13 -0600

    refactor(ui): use switch for content sanitization (#45)

--- packages/ui/src/content.ts
+++ packages/ui/src/content.ts
@@ -8,30 +8,31 @@ import { unified } from 'unified'
 export type ContentFormat = 'plain' | 'markdown' | 'html'
 
 export async function sanitizeContent(content: string, format: ContentFormat) {
-  if (format === 'html') {
-    const output = await unified()
-      .use(rehypeParse)
-      .use(rehypeSanitize, {
-        attributes: {
-          '*': ['className'],
-        },
-      })
-      .use(rehypeStringify)
-      .process(content)
+  switch (format) {
+    case 'html': {
+      const output = await unified()
+        .use(rehypeParse)
+        .use(rehypeSanitize, {
+          attributes: {
+            '*': ['className'],
+          },
+        })
+        .use(rehypeStringify)
+        .process(content)
 
-    return output.toString()
+      return output.toString()
+    }
+    case 'markdown': {
+      const output = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeSanitize)
+        .use(rehypeStringify)
+        .process(content)
+
+      return output.toString()
+    }
+    default:
+      return content
   }
-
-  if (format === 'markdown') {
-    const output = await unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeSanitize)
-      .use(rehypeStringify)
-      .process(content)
-
-    return output.toString()
-  }
-
-  return content
 }
