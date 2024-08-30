3f446c959a2dc8b20f2c27b2b198c75cd906de5e
J. Eric Bukowski <eric@byteslice.co>
2024-08-30 12:01:50 -0600

    feat(ui): eliminate unnecessary try/catch

--- packages/ui/src/deepdish.tsx
+++ packages/ui/src/deepdish.tsx
@@ -15,30 +15,26 @@ export async function DeepDish<V>(props: {
     return props.render(props.fallback)
   }
 
-  try {
-    const contractResult = getContract(props.type)
-    if (contractResult.failure) {
-      return props.render(props.fallback)
-    }
-
-    const { resolver } = contractResult.data
-    const result = await resolver.read({
-      key: props.deepdish.key,
-    })
-    if (result.failure) {
-      switch (result.failure.type) {
-        case 'DATA_MISSING':
-          // TODO: handle missing data
-          break
-        case 'DATA_INVALID':
-          // TODO: handle invalid data
-          break
-      }
-      return props.render(props.fallback)
-    }
-
-    return props.render(result.data as V)
-  } catch (ex) {
-    // TODO: handle exception
+  const contractResult = getContract(props.type)
+  if (contractResult.failure) {
+    return props.render(props.fallback)
   }
+
+  const { resolver } = contractResult.data
+  const result = await resolver.read({
+    key: props.deepdish.key,
+  })
+  if (result.failure) {
+    switch (result.failure.type) {
+      case 'DATA_MISSING':
+        // TODO: handle missing data
+        break
+      case 'DATA_INVALID':
+        // TODO: handle invalid data
+        break
+    }
+    return props.render(props.fallback)
+  }
+
+  return props.render(result.data as V)
 }
