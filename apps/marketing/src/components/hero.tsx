365a4884c63fe095b76d9213877391d08c323d27
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-09-13 18:45:26 -0600

    ci: augment continuous integration workflow (#55)

--- apps/marketing/src/components/hero.tsx
+++ apps/marketing/src/components/hero.tsx
@@ -1,5 +1,5 @@
 import { Heading1, Paragraph } from '@deepdish/ui/typography'
-import { Button } from './ui/button'
+// import { Button } from './ui/button'
 
 export default function Hero() {
   return (
