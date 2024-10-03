c144cf3ab6ea2e1fd51b04f8bb4c6fbb9d7db4d3
Dylan Slack <dylan@byteslice.co>
2024-10-03 09:27:33 -0600

    feat(menu): improve editor accessibility controls (#73)

--- packages/ui/src/menu/editor.tsx
+++ packages/ui/src/menu/editor.tsx
@@ -15,9 +15,7 @@ export function Editor(props: EditorProps) {
   const [value, setValue] = useState<string>(props.value || '')
   const [loading, setLoading] = useState<boolean>(false)
 
-  const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
+  const handleUpdate = async () => {
     try {
       setLoading(true)
       await props.onUpdate(value)
@@ -32,12 +30,27 @@ export function Editor(props: EditorProps) {
   }
 
   return (
-    <form className={styles.wrapper} onSubmit={handleUpdate}>
+    <form
+      className={styles.wrapper}
+      onSubmit={(e) => {
+        e.preventDefault()
+        handleUpdate()
+      }}
+    >
       <textarea
         value={value}
         disabled={loading}
         className={styles.textarea}
         onChange={(e) => setValue(e.currentTarget.value)}
+        onKeyDown={(e) => {
+          // NB: ensures sub menu isn't closed when arrow keys are pressed
+          e.stopPropagation()
+
+          if (e.metaKey && e.key === 'Enter') {
+            e.preventDefault()
+            handleUpdate()
+          }
+        }}
       />
       <Button type="submit" disabled={loading} loading={loading}>
         {loading ? 'Saving...' : 'Save'}
