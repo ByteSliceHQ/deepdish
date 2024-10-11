bdb541d009cb716f7d6a3346fccef363ed883b16
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-10-11 13:49:32 -0600

    refactor(ui): subsume configuration package (#80)

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig([
   {
-    entry: ['src/content.ts'],
+    entry: ['src/config/config.ts', 'src/content.ts', 'src/schemas.ts'],
     format: ['esm'],
     sourcemap: true,
     dts: true,
   },
   {
     entry: [
-      'src/config.ts',
       'src/deepdish.tsx',
       'src/elements/link.tsx',
       'src/elements/media.tsx',
