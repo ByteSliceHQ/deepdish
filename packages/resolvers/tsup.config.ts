8d13534b38c4b6632048b13dc25c8e9b6b3868c0
J. Eric Bukowski <eric@byteslice.co>
2024-10-11 19:21:20 -0600

    build: format esm only

--- packages/resolvers/tsup.config.ts
+++ packages/resolvers/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/resolver.ts', 'src/json.ts'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   sourcemap: true,
   dts: true,
 })
