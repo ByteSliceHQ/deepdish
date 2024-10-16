585d725507e13da820cb755b67321dbafd3a1178
J. Eric Bukowski <eric@byteslice.co>
2024-10-16 11:18:46 -0600

    refactor(resolvers): specify key and path types

--- packages/resolvers/src/json.ts
+++ packages/resolvers/src/json.ts
@@ -2,21 +2,24 @@ import { readFile, writeFile } from 'node:fs/promises'
 import type { ZodTypeAny } from 'zod'
 import { createResolver } from './resolver'
 
-async function parseJson(path: string) {
+type Key = string
+type Path = string
+
+async function parseJson(path: Path) {
   const json = await readFile(path, { encoding: 'utf-8' })
   return JSON.parse(json)
 }
 
-function updateJson<V>(path: string) {
-  return async (key: string, value: V) => {
+function updateJson<V>(path: Path) {
+  return async (key: Key, value: V) => {
     const data = await parseJson(path)
     data[key] = value
     return writeFile(path, JSON.stringify(data), { encoding: 'utf-8' })
   }
 }
 
-function loadValues(path: string) {
-  return async (keys: readonly string[]) => {
+function loadValues(path: Path) {
+  return async (keys: readonly Key[]) => {
     const data = await parseJson(path)
     return keys.map((key) => data[key] ?? undefined)
   }
@@ -25,7 +28,7 @@ function loadValues(path: string) {
 /** Creates a resolver to asynchronously read/write values of a JSON file. */
 export function createJsonResolver<S extends ZodTypeAny>(
   schema: S,
-  path: string,
+  path: Path,
 ) {
   return createResolver(schema)(loadValues(path), updateJson(path))
 }
