b6b988fbc2baf760b871fc282886edca70922bdc
J. Eric Bukowski <82278757+electric-gizzard@users.noreply.github.com>
2024-10-24 11:19:45 -0600

    feat(marketing): utilize cloud configuration (#107)

---
+++ .changeset/green-moose-prove.md
@@ -0,0 +1,5 @@
+---
+"@deepdish/marketing": patch
+---
+
+Utilized cloud configuration.
--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -1,20 +1,23 @@
 import '../globals.css'
 
 import { cn } from '@/lib/utils'
-import { cms } from '@deepdish-cloud/config'
+import { cloudConfig } from '@deepdish-cloud/config'
 import { init } from '@deepdish-cloud/config/client'
+import { configure } from '@deepdish/ui/config'
 import { Workbench } from '@deepdish/workbench'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-cms({
-  contentUrl: process.env.DEEPDISH_URL,
-  oauthClientId: process.env.CLERK_OAUTH_CLIENT_ID,
-  oauthRedirectUrl: process.env.CLERK_OAUTH_REDIRECT_URL,
-  oauthUrl: process.env.DEEPDISH_OAUTH_URL,
-  secretKey: process.env.DEEPDISH_SECRET_KEY,
-  projectAlias: process.env.DEEPDISH_PROJECT_ALIAS,
-})
+configure(
+  cloudConfig({
+    contentUrl: process.env.DEEPDISH_URL,
+    oauthClientId: process.env.CLERK_OAUTH_CLIENT_ID,
+    oauthRedirectUrl: process.env.CLERK_OAUTH_REDIRECT_URL,
+    oauthUrl: process.env.DEEPDISH_OAUTH_URL,
+    secretKey: process.env.DEEPDISH_SECRET_KEY,
+    projectAlias: process.env.DEEPDISH_PROJECT_ALIAS,
+  }),
+)
 
 const fontSans = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
