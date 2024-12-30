d2a0ee11cc9d6716c744d3ce5ba5d25c9ea6536a
CJ Brewer <brewercalvinj@gmail.com>
2024-12-30 12:56:31 -0700

    chore: lint

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
 import { AppLayout } from '@/components/app-layout'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
