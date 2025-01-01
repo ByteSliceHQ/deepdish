f809244ca5c968de0d2d8d39589240982ff0dbf7
J. Eric Bukowski <eric@byteslice.co>
2024-12-31 18:00:45 -0700

    chore(marketing): eliminate extraneous variables from process.env definition

--- apps/marketing/environment.d.ts
+++ apps/marketing/environment.d.ts
@@ -1,12 +1,7 @@
 namespace NodeJS {
   interface ProcessEnv {
-    CLERK_OAUTH_CLIENT_ID: string
-    CLERK_OAUTH_REDIRECT_URL: string
-    CLERK_URL: string
-    DEEPDISH_OAUTH_URL: string
+    DEEPDISH_MODE: 'draft' | 'live'
     DEEPDISH_PROJECT_ALIAS: string
-    DEEPDISH_RESOLVER: 'local' | 'cloud'
     DEEPDISH_SECRET_KEY: string
-    DEEPDISH_URL: string
   }
 }
