0f4c94fb77ee7c9c25f9b689721f4fa59fbfbce7
J. Eric Bukowski <eric@byteslice.co>
2025-01-14 18:20:52 -0700

    chore(marketing): add type declaration file for environment variables

---
+++ apps/marketing/environment.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DEEPDISH_CLOUD_ENDPOINT: string
+    DEEPDISH_CLOUD_OAUTH_CLIENT_ID: string
+    DEEPDISH_CLOUD_OAUTH_REDIRECT_URI: string
+    DEEPDISH_CLOUD_STATE: string
+    DEEPDISH_PROJECT_ALIAS: string
+    DEEPDISH_SECRET_KEY: string
+  }
+}
