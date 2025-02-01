77c780fa45ad6db5dc55b9d6757276f69ddbc083
J. Eric Bukowski <eric@byteslice.co>
2025-02-01 09:12:12 -0700

    style(core): modify suppressed rule explanation

--- packages/core/src/shell.tsx
+++ packages/core/src/shell.tsx
@@ -5,7 +5,7 @@ import { useMode } from './context'
 export function Shell(props: {
   children: React.ReactNode
 }) {
-  // biome-ignore lint/correctness/noUnusedVariables: "mode" state will be utilized in the future
+  // biome-ignore lint/correctness/noUnusedVariables: "mode" state will soon be utilized
   const mode = useMode()
 
   return <>{props.children}</>
