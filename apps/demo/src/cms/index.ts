a3d170bb6072c0cd3af1f57064237916e7280688
J. Eric Bukowski <eric@byteslice.co>
2025-02-04 22:12:28 -0700

    feat(demo): access environment variables during configuration

--- apps/demo/src/cms/index.ts
+++ apps/demo/src/cms/index.ts
@@ -27,7 +27,10 @@ export async function cms() {
   await init()
 
   return await configure({
-    baseUrl: process.env.BASE_URL,
+    settings: {
+      baseUrl: process.env.BASE_URL,
+      draft: process.env.DEEPDISH_MODE === 'draft',
+    },
     contracts: {
       typography: {
         resolver: createJsonResolver(contentPath, typographySchema, {
