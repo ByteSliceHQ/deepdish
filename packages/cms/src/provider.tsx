5a3f0779abf8c66d387bd252468254a3d98048c8
J. Eric Bukowski <eric@byteslice.co>
2025-02-12 09:21:52 -0700

    feat(cms): render workbench in draft mode

--- packages/cms/src/provider.tsx
+++ packages/cms/src/provider.tsx
@@ -3,14 +3,19 @@
 import { DeepDishProvider } from '@deepdish/core/context'
 import { Workbench } from '@deepdish/workbench'
 
-export function Provider({
-  children,
-  title,
-}: { children: React.ReactNode; title?: string }) {
-  return (
-    <DeepDishProvider>
-      {children}
-      <Workbench title={title} />
-    </DeepDishProvider>
-  )
+export function Provider(props: {
+  children: React.ReactNode
+  draft: boolean
+  title?: string
+}) {
+  if (props.draft) {
+    return (
+      <DeepDishProvider>
+        {props.children}
+        <Workbench title={props.title} />
+      </DeepDishProvider>
+    )
+  }
+
+  return <DeepDishProvider>{props.children}</DeepDishProvider>
 }
