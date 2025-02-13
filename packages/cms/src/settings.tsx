fdc0985cd5f7212ff9ec5e3b9e5cb31d3a81dfb8
J. Eric Bukowski <eric@byteslice.co>
2025-02-12 18:23:51 -0700

    fix(cms): include title prop in settings

--- packages/cms/src/settings.tsx
+++ packages/cms/src/settings.tsx
@@ -1,9 +1,13 @@
 import { getSettings } from '@deepdish/ui/config'
 import { Provider } from './provider'
 
-export function Settings(props: { children: React.ReactNode }) {
+export function Settings(props: { children: React.ReactNode; title?: string }) {
   const settings = getSettings()
   const draft = settings.failure ? false : settings.data.draft
 
-  return <Provider draft={draft}>{props.children}</Provider>
+  return (
+    <Provider draft={draft} title={props.title}>
+      {props.children}
+    </Provider>
+  )
 }
