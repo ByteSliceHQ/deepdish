8e103c0b107a8f62f7aa9a912a5b53b2c8ef18d2
J. Eric Bukowski <eric@byteslice.co>
2025-02-13 20:03:41 -0700

    refactor(cms): create provider subdirectory

--- packages/cms/src/cms.ts
+++ packages/cms/src/cms.ts
@@ -1,4 +1,4 @@
 import { configure as deepdish } from '@deepdish/ui/config'
-import { ProviderContainer as DeepDishProvider } from './container'
+import { ProviderContainer as DeepDishProvider } from './provider/container'
 
 export { deepdish, DeepDishProvider }
