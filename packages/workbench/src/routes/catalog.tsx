85cbcf7ce29765c4198cb9befd327222e53fca5a
Dylan Slack <dylanslack@gmail.com>
2025-02-15 12:42:06 -0700

    chore: lint

--- packages/workbench/src/routes/catalog.tsx
+++ packages/workbench/src/routes/catalog.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
 import { EmptyState } from '@/components/empty-state'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/catalog')({
   component: RouteComponent,
