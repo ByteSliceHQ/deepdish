9fec65a8ef8ad93373f2037132c0cc470135dac1
CJ Brewer <brewercalvinj@gmail.com>
2025-02-21 13:54:58 -0700

    fix: add workbench

--- packages/cms/src/provider.tsx
+++ packages/cms/src/provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { DeepDishProvider } from '@deepdish/core/context'
+import { Workbench } from '@deepdish/workbench'
 
 export function Provider(props: {
   children: React.ReactNode
@@ -9,7 +10,12 @@ export function Provider(props: {
   authDisabled?: boolean
 }) {
   if (props.draft) {
-    return <DeepDishProvider>{props.children}</DeepDishProvider>
+    return (
+      <DeepDishProvider>
+        {props.children}
+        <Workbench title={props.title} authDisabled={props.authDisabled} />
+      </DeepDishProvider>
+    )
   }
 
   return <DeepDishProvider>{props.children}</DeepDishProvider>
