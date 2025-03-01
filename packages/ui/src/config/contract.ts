bdb3f55fa9ec6bb193b2bca345f5de9226ceb498
J. Eric Bukowski <eric@byteslice.co>
2025-03-01 11:49:08 -0700

    refactor(contract): remove readonly enforcement

--- packages/ui/src/config/contract.ts
+++ packages/ui/src/config/contract.ts
@@ -3,13 +3,11 @@ import type { ZodTypeAny, z } from 'zod'
 
 export type Schema = ZodTypeAny
 
-type Value<S extends Schema> = z.infer<S>
+export type Value<S extends Schema> = z.infer<S>
 
-export type Contract<S extends Schema> = Readonly<{
+export type Contract<S extends Schema> = {
   resolver: Resolver<Value<S>>
   schema: S
-}>
+}
 
-export type Contracts = Readonly<{
-  [key in string]?: Contract<Schema>
-}>
+export type Contracts = Record<string, Contract<Schema>>
