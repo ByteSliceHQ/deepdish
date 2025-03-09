e965e6f4c93b8cfcdffb8f5125419d2906d0faac
J. Eric Bukowski <eric@byteslice.co>
2025-03-09 15:48:26 -0600

    feat(ui): remove pre-built elements

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -9,13 +9,7 @@ export default defineConfig([
     dts: true,
   },
   {
-    entry: [
-      'src/config/components.tsx',
-      'src/deepdish.tsx',
-      'src/elements/link.tsx',
-      'src/elements/media.tsx',
-      'src/elements/typography.tsx',
-    ],
+    entry: ['src/config/components.tsx', 'src/deepdish.tsx'],
     format: ['esm'],
     sourcemap: true,
     dts: true,
