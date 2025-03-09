6f1cd89cc667e698e3a41569dc1e2c1692f752f5
J. Eric Bukowski <eric@byteslice.co>
2025-03-09 16:15:52 -0600

    refactor(demo): use generated components

--- apps/demo/src/app/page.tsx
+++ apps/demo/src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Feature } from '@/feature'
-import { Paragraph } from '@deepdish/ui/typography'
 import { components } from './layout'
 
 const Text = components.text
@@ -8,15 +7,23 @@ export default function Demo() {
   return (
     <div className="flex flex-col px-8 py-6 gap-4">
       <div className="flex flex-col gap-1 max-w-prose">
-        <Paragraph className="text-xl font-bold" deepdish={{ key: 'headline' }}>
-          DeepDish Demo
-        </Paragraph>
-        <Paragraph deepdish={{ key: 'sub-headline' }}>
-          DeepDish is an alternative to traditional CMS systems. The code looks
+        <Text
+          deepdish={{ key: 'headline' }}
+          fallback="DeepDish Demo"
+          render={async (value) => {
+            return <p className="text-xl font-bold">{value}</p>
+          }}
+        />
+        <Text
+          deepdish={{ key: 'sub-headline' }}
+          fallback="DeepDish is an alternative to traditional CMS systems. The code looks
           like normal React, but every element is editable. First, log in with
           the Workbench below. Then, simply right-click on elements and edit the
-          text.
-        </Paragraph>
+          text."
+          render={async (value) => {
+            return <p>{value}</p>
+          }}
+        />
       </div>
       <div className="grid grid-cols-3 gap-4">
         <div>
@@ -42,19 +49,23 @@ export default function Demo() {
         </div>
         <div>
           <p className="font-bold">Static collection</p>
-          <Paragraph
-            deepdish={{
-              collection: ['collection-1', 'collection-2'],
+          <Text
+            deepdish={{ collection: ['collection-1', 'collection-2'] }}
+            fallback="This is a static collection."
+            render={async (value) => {
+              return <p>{value}</p>
             }}
-          >
-            This is a static collection.
-          </Paragraph>
+          />
         </div>
         <div>
           <p className="font-bold">Dynamic collection</p>
-          <Paragraph deepdish={{ collection: '*' }}>
-            This is a dynamic collection.
-          </Paragraph>
+          <Text
+            deepdish={{ collection: '*' }}
+            fallback="This is a dynamic collection."
+            render={async (value) => {
+              return <p>{value}</p>
+            }}
+          />
         </div>
       </div>
       <div>
