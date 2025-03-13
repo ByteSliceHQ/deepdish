25ce659abd2db9d958091191f4d0b451ff8357af
J. Eric Bukowski <eric@byteslice.co>
2025-03-13 13:02:13 -0600

    feat(demo): inject write handlers

--- apps/demo/src/feature.tsx
+++ apps/demo/src/feature.tsx
@@ -1,7 +1,10 @@
 import { components } from '@/app/layout'
+import { contracts } from '@/cms'
 import type { DeepDishProps } from '@deepdish/ui/deepdish'
+import { headers } from 'next/headers'
 
 const Component = components.feature
+const contract = contracts.feature
 
 export function Feature(props: {
   deepdish: DeepDishProps
@@ -9,6 +12,11 @@ export function Feature(props: {
   return (
     <Component
       deepdish={props.deepdish}
+      onUpdate={async (key, value) => {
+        'use server'
+
+        await contract.resolver.write({ key, headers: await headers() }, value)
+      }}
       render={async (value) => {
         return (
           <div className="border border-gray-200 rounded-lg p-4 bg-white shadow-sm inline-block">
--- apps/demo/src/text.tsx
+++ apps/demo/src/text.tsx
@@ -1,10 +1,21 @@
 import { components } from '@/app/layout'
-import type { ComponentProps } from 'react'
+import { contracts } from '@/cms'
+import { headers } from 'next/headers'
 
 const Component = components.text
+const contract = contracts.text
 
-type TextProps = ComponentProps<typeof Component>
+type TextProps = Omit<React.ComponentProps<typeof Component>, 'onUpdate'>
 
 export function Text(props: TextProps) {
-  return <Component {...props} />
+  return (
+    <Component
+      {...props}
+      onUpdate={async (key, value) => {
+        'use server'
+
+        await contract.resolver.write({ key, headers: await headers() }, value)
+      }}
+    />
+  )
 }
