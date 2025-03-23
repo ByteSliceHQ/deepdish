2ef2aa5608691339b23750f738631a1f0b795364
J. Eric Bukowski <eric@byteslice.co>
2025-03-23 16:45:22 -0600

    feat(cms): modify base url helper to accept no arg

--- packages/cms/src/vercel/index.ts
+++ packages/cms/src/vercel/index.ts
@@ -1,12 +1,14 @@
-export function getBaseUrl({
-  productionBaseProtocol = 'https',
-  previewBaseProtocol = 'https',
-  localhost = 'http://localhost:3000',
-}: {
+type BaseUrlConfig = {
   productionBaseProtocol?: 'https' | 'http'
   previewBaseProtocol?: 'https' | 'http'
   localhost?: string
-}) {
+}
+
+export function getBaseUrl(config?: BaseUrlConfig) {
+  const productionBaseProtocol = config?.productionBaseProtocol ?? 'https'
+  const previewBaseProtocol = config?.previewBaseProtocol ?? 'https'
+  const localhost = config?.localhost ?? 'http://localhost:3000'
+
   // biome-ignore lint/nursery/noProcessEnv: Required for Vercel integration
   switch (process.env.VERCEL_ENV) {
     case 'production':
