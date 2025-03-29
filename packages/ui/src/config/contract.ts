f3ad5d26fe7d53cb46427dee17f7fe9cbb782fb9
J. Eric Bukowski <eric@byteslice.co>
2025-03-29 11:30:16 -0600

    feat(config): add optional components to contract

--- packages/ui/src/config/contract.ts
+++ packages/ui/src/config/contract.ts
@@ -1,19 +1,24 @@
 import type { Schema, Value } from '@deepdish/core/schema'
 import type { Resolver } from '@deepdish/resolvers'
 
-export type Contract<S extends Schema> = {
-  resolver: Resolver<Value<S>>
+type Component<S extends Schema> = (value: Value<S>) => JSX.Element
+
+export type Contract<S extends Schema, K extends string = string> = {
   schema: S
+  resolver: Resolver<Value<S>>
+  components?: Record<K, Component<S>>
 }
 
 export type Contracts = Record<string, Contract<Schema>>
 
-export function createContract<V>(
-  schema: Schema<V>,
-  resolver: Resolver<V>,
-): Contract<Schema<V>> {
+export function createContract<S extends Schema, K extends string>(
+  schema: S,
+  resolver: Resolver<Value<S>>,
+  components?: Record<K, Component<S>>,
+): Contract<S, K> {
   return {
     schema,
     resolver,
+    components,
   }
 }
