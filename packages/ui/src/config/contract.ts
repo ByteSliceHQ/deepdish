acce8f6896a5b8d426fd34b3d691286ed19f8728
J. Eric Bukowski <eric@byteslice.co>
2025-04-05 11:27:45 -0600

    Revert "feat(components): create element components in contract namespace"
    This reverts commit 03402f569b52878e4fe9dd3e3f48271d93c6ffae.

--- packages/ui/src/config/contract.ts
+++ packages/ui/src/config/contract.ts
@@ -1,31 +1,20 @@
 import type { Schema, Value } from '@deepdish/core/schema'
 import type { Resolver } from '@deepdish/resolvers'
 
-export type Render<S extends Schema, E extends HTMLElement> = (
-  props: React.HTMLAttributes<E>,
-  value?: Value<S>,
-) => Promise<React.ReactElement>
+type Component<S extends Schema> = (value: Value<S>) => JSX.Element
 
-export type Contract<
-  S extends Schema = Schema,
-  K extends string = string,
-  E extends HTMLElement = HTMLElement,
-> = {
+export type Contract<S extends Schema = Schema, K extends string = string> = {
   schema: S
   resolver: Resolver<Value<S>>
-  components: Record<K, Render<S, E>>
+  components: Record<K, Component<S>>
 }
 
 export type Contracts = Record<string, Contract>
 
-export function createContract<
-  S extends Schema,
-  K extends string,
-  E extends HTMLElement,
->(
+export function createContract<S extends Schema, K extends string>(
   schema: S,
   resolver: Resolver<Value<S>>,
-  components: Record<K, Render<S, E>>,
+  components: Record<K, Component<S>>,
 ): Contract<S, K> {
   return {
     schema,
