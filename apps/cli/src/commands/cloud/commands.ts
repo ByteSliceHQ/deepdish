2417aa2b8cd8dc301bc316b4cee53182b0c48dfc
CJ Brewer <brewercalvinj@gmail.com>
2025-06-22 12:02:08 -0600

    feat(cli): rename deepdish cli package (#239)

--- apps/cli/src/commands/cloud/commands.ts
+++ apps/cli/src/commands/cloud/commands.ts
@@ -1,14 +1,12 @@
 import { buildRouteMap } from '@stricli/core'
 import { cloudAuthRoutes } from './auth/commands'
 import { cloudBillingRoutes } from './billing/commands'
-import { cloudKeyRoutes } from './key/commands'
 import { cloudProjectRoutes } from './project/commands'
 
 export const cloudRoutes = buildRouteMap({
   routes: {
     auth: cloudAuthRoutes,
     billing: cloudBillingRoutes,
-    key: cloudKeyRoutes,
     project: cloudProjectRoutes,
   },
   docs: {
